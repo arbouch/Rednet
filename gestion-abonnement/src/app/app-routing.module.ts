ba4a476dbb17215844754e7dea800862b4d346df
Mohamed Arbi <mohamedarbi458@gmail.com>
2020-11-13 21:10:54 +0100

    update app routing

--- gestion-abonnement/src/app/app-routing.module.ts
+++ gestion-abonnement/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 import { Routes, RouterModule } from '@angular/router';
+import { AbonnementsDetailsComponent } from './abonnements-details/abonnements-details.component';
+import { AbonnesDetailsComponent } from './abonnes-details/abonnes-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+    children: [
+    {path: 'abonne',
+    component: AbonnesDetailsComponent
+    },
+    {path: 'abonnement',
+    component: AbonnementsDetailsComponent
+    },
+    
+    
+    ]
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
