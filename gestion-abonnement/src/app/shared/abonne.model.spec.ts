52a04b7b294342a649936f0458f6d0dc25304d31
Mohamed Arbi <mohamedarbi458@gmail.com>
2020-12-11 22:18:55 +0100

    fix shared folder

--- gestion-abonnement/src/app/shared/abonne.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Abonne } from './abonne.model';
-
-describe('Abonne', () => {
-  it('should create an instance', () => {
-    expect(new Abonne()).toBeTruthy();
-  });
-});
--- gestion-abonnement/src/app/shared/abonnement.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Abonnement } from './abonnement.model';
-
-describe('Abonnement', () => {
-  it('should create an instance', () => {
-    expect(new Abonnement()).toBeTruthy();
-  });
-});
